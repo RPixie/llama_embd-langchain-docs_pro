e13053b44c1d6a6b7269434f31694435bc2cd429
RPixie <106712904+RPixie@users.noreply.github.com>
2022-01-22 18:44:27 +0000

    Add FAQ

--- src/HELPERS/step_1_save_chunked_docs.py
+++ src/HELPERS/step_1_save_chunked_docs.py
@@ -39,3 +39,21 @@ def save_documents(
         - documents (List[Dict[str, Union[str, List[Dict[str, str]]]]]):
             A list of objects, where each object has two properties:
                 - the name of the document that was chunked,
+                - and the chunked data itself.
+        - save_json_chunks_directory (str): The path to the directory where the JSON files will be saved.
+
+    Returns:
+        - None
+    """
+
+    # Create directory for chunked data if it doesn't exist
+    if not os.path.exists(save_json_chunks_directory):
+        os.makedirs(save_json_chunks_directory)
+
+    # Save documents to JSON file with dynamic name
+    for doc in documents:
+        json_file_path = os.path.join(
+            save_json_chunks_directory, f"{doc['name']} Chunks.json"
+        )
+        with open(json_file_path, "w") as f:
+            json.dump(doc["chunks"], f)
