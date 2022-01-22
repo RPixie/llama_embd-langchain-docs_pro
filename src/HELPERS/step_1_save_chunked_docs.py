a2fe216111008a22d50d5542f4aa970ba8536baa
RPixie <106712904+RPixie@users.noreply.github.com>
2022-01-22 06:03:05 +0000

    try to fix

--- src/HELPERS/step_1_save_chunked_docs.py
+++ src/HELPERS/step_1_save_chunked_docs.py
@@ -13,3 +13,29 @@
     and to save the documents to JSON files with dynamic names. 
     
     The resulting JSON files are saved in the directory specified by the save_json_chunks_directory argument.
+"""
+
+import os
+import json
+
+
+from typing import List, Dict, Union
+
+
+def save_documents(
+    documents: List[Dict[str, Union[str, List[Dict[str, str]]]]],
+    save_json_chunks_directory: str,
+) -> None:
+    """
+    Saves a list of objects to JSON files. Each object in the list should have two properties:
+        - the name of the document that was chunked,
+        - and the chunked data itself.
+
+    The JSON file should be named after the document name, with "Chunks"
+    appended to the end of the name.
+    The content of the JSON file should be the chunked data.
+
+    Args:
+        - documents (List[Dict[str, Union[str, List[Dict[str, str]]]]]):
+            A list of objects, where each object has two properties:
+                - the name of the document that was chunked,
