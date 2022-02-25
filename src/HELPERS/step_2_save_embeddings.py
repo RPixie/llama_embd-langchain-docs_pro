cf45a894024b90c83d6bb0c1b23f9fa8a309a9d9
RPixie <106712904+RPixie@users.noreply.github.com>
2022-02-25 14:22:26 +0000

    add gifs

--- src/HELPERS/step_2_save_embeddings.py
+++ src/HELPERS/step_2_save_embeddings.py
@@ -2,4 +2,5 @@
     This function takes in three parameters: 
     
     "embeddings" which is an instance of the "Embeddings" class, 
-    "file_name" which is a string representing the n
+    "file_name" which is a string representing the name of the file to be saved, and 
+  
