dca02d88d7884d4f95dd189ffad3548f299e7e58
RPixie <106712904+RPixie@users.noreply.github.com>
2022-03-19 08:34:03 +0000

    test:coverage

--- src/HELPERS/step_2_save_embeddings.py
+++ src/HELPERS/step_2_save_embeddings.py
@@ -3,4 +3,4 @@
     
     "embeddings" which is an instance of the "Embeddings" class, 
     "file_name" which is a string representing the name of the file to be saved, and 
-    "directory_path" which is a string representing the path to the directo
+    "directory_path" which is a string representing the path to the directory where the file will be save
