cfc01f070949a4a487ad4f083168c03228ba8463
RPixie <106712904+RPixie@users.noreply.github.com>
2022-03-19 10:39:13 +0000

    Pontoon: Update Upper Sorbian (hsb) localization of AMO

--- src/HELPERS/step_2_save_embeddings.py
+++ src/HELPERS/step_2_save_embeddings.py
@@ -3,4 +3,6 @@
     
     "embeddings" which is an instance of the "Embeddings" class, 
     "file_name" which is a string representing the name of the file to be saved, and 
-    "directory_path" which is a string representing the path to the directory where the file will be save
+    "directory_path" which is a string representing the path to the directory where the file will be saved.
+
+    The function fi
