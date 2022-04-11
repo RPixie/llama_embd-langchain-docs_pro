a723f74a1636daf0a0061f335cd839f2396f412a
RPixie <106712904+RPixie@users.noreply.github.com>
2022-04-11 17:13:12 +0000

    additional test cases

--- src/HELPERS/step_2_save_embeddings.py
+++ src/HELPERS/step_2_save_embeddings.py
@@ -7,4 +7,4 @@
 
     The function first creates a directory at the specified path if it does not already exist. 
     It then creates a file path by joining the directory path and file name with a ".pkl" extension. 
-    Finally, it saves the embedding
+    Finally, it saves the embeddings object to the binar
