e0b7667102d4586be368c5c619d37e73a33f0c52
RPixie <106712904+RPixie@users.noreply.github.com>
2022-04-11 09:45:08 +0000

    Add files to gitignore

--- src/HELPERS/step_2_save_embeddings.py
+++ src/HELPERS/step_2_save_embeddings.py
@@ -7,3 +7,4 @@
 
     The function first creates a directory at the specified path if it does not already exist. 
     It then creates a file path by joining the directory path and file name with a ".pkl" extension. 
+    Finally, it saves the embedding
