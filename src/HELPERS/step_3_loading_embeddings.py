429b7b637f26466bee8a6c9268b8b5c2635a7ea1
RPixie <106712904+RPixie@users.noreply.github.com>
2022-07-28 15:06:25 +0000

    Daily

--- src/HELPERS/step_3_loading_embeddings.py
+++ src/HELPERS/step_3_loading_embeddings.py
@@ -1,4 +1,5 @@
 """
     This code defines a function called load_embeddings that loads embeddings from a file using the pickle module. 
         
-    The function takes one ar
+    The function takes one argument: 
+        file_path whi
