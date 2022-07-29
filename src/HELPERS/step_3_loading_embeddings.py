1365ebed703a11e5bb3a1ffee02022e91b391027
RPixie <106712904+RPixie@users.noreply.github.com>
2022-07-29 08:08:05 +0000

    Update CustomHosts

--- src/HELPERS/step_3_loading_embeddings.py
+++ src/HELPERS/step_3_loading_embeddings.py
@@ -2,4 +2,4 @@
     This code defines a function called load_embeddings that loads embeddings from a file using the pickle module. 
         
     The function takes one argument: 
-        file_path whi
+        file_path which is the path to the file con
