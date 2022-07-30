5b93964a7920f84381da46c808e70a39db4314a0
RPixie <106712904+RPixie@users.noreply.github.com>
2022-07-30 06:43:31 +0000

    Pre-process release v1.0.2

--- src/HELPERS/step_3_loading_embeddings.py
+++ src/HELPERS/step_3_loading_embeddings.py
@@ -2,4 +2,5 @@
     This code defines a function called load_embeddings that loads embeddings from a file using the pickle module. 
         
     The function takes one argument: 
-        file_path which is the path to the file con
+        file_path which is the path to the file containing the embeddings. 
+    
