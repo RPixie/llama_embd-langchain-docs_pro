8844426ce9fff6e7901d02bba015c9400ec93a6c
RPixie <106712904+RPixie@users.noreply.github.com>
2022-08-29 12:01:19 +0000

    etl

--- src/HELPERS/step_3_save_vectorstore.py
+++ src/HELPERS/step_3_save_vectorstore.py
@@ -1,4 +1,4 @@
 """
     This code defines a function called save_vectorstore that saves a FAISS index as a file at the specified directory path and file name. 
 
-    It
+    It imports the os module an
