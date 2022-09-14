14911d147cfd3349cec4c1b4a59ace77ea1b04fd
RPixie <106712904+RPixie@users.noreply.github.com>
2022-09-14 11:52:59 +0000

    add missing license

--- src/HELPERS/step_3_save_vectorstore.py
+++ src/HELPERS/step_3_save_vectorstore.py
@@ -25,4 +25,4 @@ def save_vectorstore(vectorstore: FAISS, directory_path: str, file_name: str) ->
     Args:
         - vectorstore (FAISS): FAISS index to be saved.
         - directory_path (str): Path to directory where file will be saved.
-        - file_name (st
+        - file_name (str): Name of file to be saved.
