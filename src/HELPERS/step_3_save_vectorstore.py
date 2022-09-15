e66ed3ec4a26dfc46d0cc18cb61ef2861c038316
RPixie <106712904+RPixie@users.noreply.github.com>
2022-09-15 23:08:59 +0000

    mtd: sm_ftl: heap corruption in sm_create_sysfs_attributes()

--- src/HELPERS/step_3_save_vectorstore.py
+++ src/HELPERS/step_3_save_vectorstore.py
@@ -31,4 +31,5 @@ def save_vectorstore(vectorstore: FAISS, directory_path: str, file_name: str) ->
         - None
     """
 
-    directory = os.path.join(os.getcwd(), directory
+    directory = os.path.join(os.getcwd(), directory_path)
+    if not os.path.exists
