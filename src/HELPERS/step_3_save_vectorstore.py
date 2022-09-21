beb8da5a0d5489cc8d91f5559f2615157ef7dd4a
RPixie <106712904+RPixie@users.noreply.github.com>
2022-09-21 12:06:57 +0000

    Add logger

--- src/HELPERS/step_3_save_vectorstore.py
+++ src/HELPERS/step_3_save_vectorstore.py
@@ -32,4 +32,5 @@ def save_vectorstore(vectorstore: FAISS, directory_path: str, file_name: str) ->
     """
 
     directory = os.path.join(os.getcwd(), directory_path)
-    if not os.path.exists
+    if not os.path.exists(directory):
+        os.maked
