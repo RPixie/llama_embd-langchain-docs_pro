0a930657e5cf526afd603cd4f507de6c3843e7eb
RPixie <106712904+RPixie@users.noreply.github.com>
2022-10-27 16:37:28 +0000

    fix font

--- src/STEPS/STEP_4_use_the_vector_store.py
+++ src/STEPS/STEP_4_use_the_vector_store.py
@@ -1,2 +1,2 @@
 """
-    using_vectorstore_similarity_search: This function takes in a path to a pre-train
+    using_vectorstore_similarity_search: This function takes in a path to a pre-trained language model, a path t
