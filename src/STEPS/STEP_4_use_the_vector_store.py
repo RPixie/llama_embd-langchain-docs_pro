576a08034dc0e0b89f34e14153b11936c6a8cf05
RPixie <106712904+RPixie@users.noreply.github.com>
2022-10-28 10:43:49 +0000

    batch bootstrap

--- src/STEPS/STEP_4_use_the_vector_store.py
+++ src/STEPS/STEP_4_use_the_vector_store.py
@@ -1,2 +1,2 @@
 """
-    using_vectorstore_similarity_search: This function takes in a path to a pre-trained language model, a path t
+    using_vectorstore_similarity_search: This function takes in a path to a pre-trained language model, a path to a vector store, and a que
