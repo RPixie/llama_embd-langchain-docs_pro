4b942b92239d820db2ddd2567c916ef40674d3a2
RPixie <106712904+RPixie@users.noreply.github.com>
2022-11-08 17:12:23 +0000

    more caching

--- src/STEPS/STEP_4_use_the_vector_store.py
+++ src/STEPS/STEP_4_use_the_vector_store.py
@@ -1,4 +1,4 @@
 """
     using_vectorstore_similarity_search: This function takes in a path to a pre-trained language model, a path to a vector store, and a query string. It first embeds the query text using the pre-trained language model, then loads the vector store using the FAISS library. Finally, it uses the vector store to find the k most similar documents to the query, where k is set to 4 in this implementation. The function returns a list of Document objects, where each Document represents one of the most similar documents to the query.
 
-    Q_and_A_implementation: This function takes in a path to a pre-trained language model, a list of Document objects representing 
+    Q_and_A_implementation: This function takes in a path to a pre-trained language model, a list of Document objects representing the most similar documents to a q
