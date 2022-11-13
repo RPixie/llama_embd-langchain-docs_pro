d94dfea484cbec146d9be096ee5f0dfab75c0169
RPixie <106712904+RPixie@users.noreply.github.com>
2022-11-13 22:53:23 +0000

    WORK

--- src/STEPS/STEP_4_use_the_vector_store.py
+++ src/STEPS/STEP_4_use_the_vector_store.py
@@ -1,4 +1,6 @@
 """
     using_vectorstore_similarity_search: This function takes in a path to a pre-trained language model, a path to a vector store, and a query string. It first embeds the query text using the pre-trained language model, then loads the vector store using the FAISS library. Finally, it uses the vector store to find the k most similar documents to the query, where k is set to 4 in this implementation. The function returns a list of Document objects, where each Document represents one of the most similar documents to the query.
 
-    Q_and_A_implementation: This function takes in a path to a pre-trained language model, a list of Document objects representing the most similar documents to a query, and the query string itself. It loads a pre-trained question-answering model using the load_qa_chain function from the langchain.chains.question_answering module, and applies this model to the list of Document objects and the query string to generate an answer. The function returns the answer as 
+    Q_and_A_implementation: This function takes in a path to a pre-trained language model, a list of Document objects representing the most similar documents to a query, and the query string itself. It loads a pre-trained question-answering model using the load_qa_chain function from the langchain.chains.question_answering module, and applies this model to the list of Document objects and the query string to generate an answer. The function returns the answer as a string.
+
+    The code then
