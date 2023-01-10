c1fb25e8fad9ffbf01167ccabb62bc7453c4125c
RPixie <106712904+RPixie@users.noreply.github.com>
2023-01-10 21:14:03 +0000

    Update IDxyz.js

--- src/STEPS/STEP_4_use_the_vector_store.py
+++ src/STEPS/STEP_4_use_the_vector_store.py
@@ -31,4 +31,5 @@ def using_vectorstore_similarity_search(
         query (str): The query to search for.
 
     Returns:
-        List[Document]: A list of the most similar doc
+        List[Document]: A list of the most similar documents to the query.
+    """
