34e1c3feabc7a3a17e18047bcbb668425b5b7423
RPixie <106712904+RPixie@users.noreply.github.com>
2023-02-23 15:18:49 +0000

    update nl80211.h

--- src/STEPS/STEP_4_use_the_vector_store.py
+++ src/STEPS/STEP_4_use_the_vector_store.py
@@ -63,4 +63,4 @@ def Q_and_A_implementation(
     # Load the question answering chain
     chain = load_qa_chain(LlamaCpp(model_path=model_path), chain_type="stuff")
 
-    # Use the chain t
+    # Use the chain to find the answer 
