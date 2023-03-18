704ca9bf05ddd12234aa37302a7ef931ef69f612
RPixie <106712904+RPixie@users.noreply.github.com>
2023-03-18 10:40:01 +0000

    Fix styling issue

--- src/STEPS/STEP_4_use_the_vector_store.py
+++ src/STEPS/STEP_4_use_the_vector_store.py
@@ -76,4 +76,5 @@ load_dotenv()  # Load environment variables from .env file
 path_to_ggml_model: str = os.getenv("PATH_TO_GGML_MODEL")
 
 saving_vectorstore_file_name: str = os.getenv("SAVING_VECTORSTORE_FILE_NAME")
-saving_vectorstore_directory: str = os.getenv("SAVING_VECTORSTORE_
+saving_vectorstore_directory: str = os.getenv("SAVING_VECTORSTORE_DIRECTORY")
+vectorstore_path = os.pa
